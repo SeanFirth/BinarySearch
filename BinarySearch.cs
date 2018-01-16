4e472ca8bba2f968de3d94161d71fa084057d51b
Sean <contactseanfirth@gmail.com>
2018-01-16 11:11:09 +0000

    Changes from lesson
    Re-structured the code - now a hard-coded array (but can be changed) but the binary search still works as before.

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,54 +8,43 @@ namespace BinarySearch {
     class Program {
         static void Main(string[] args) {
 
-            // Step 1 - return an array of n length
-            // Step 2 - find length of array and middle of array
 
-
-            int[] valueArray = new int[52];
+            int[] valueArray ={ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int minvalue = 0;
             int maxvalue = valueArray.Length;
             bool found = false;
             int middlePoint = valueArray.Length / 2;
-            bool Input = true;
-            int i = 0;
-
-            Console.WriteLine("Input Numbers Wanted : ");
-            while(Input) {
-
-                int InputNumber = Convert.ToInt32(Console.ReadLine());
-                valueArray[i] = InputNumber;
-                i++;
-
-                if (InputNumber == 1000000) {
-                    Input = false;
-                }
-            }
-
-            //for (int i = 0; i < valueArray.Length; i++) {
-                //valueArray[i] = i;
-            //}
+            
 
             Console.WriteLine("Number To Find :");
             int valueSearch = Convert.ToInt32(Console.ReadLine());
 
             while (!found) {
-                //Console.WriteLine(middlePoint.ToString());
+
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("Array split, the middle point of the array is: " + middlePoint.ToString());
+                
+
                 if (valueSearch == middlePoint) {
                     found = true;
-                    Console.WriteLine("Found In Location " + middlePoint + " In Array");
-                } else if (valueSearch < middlePoint & valueSearch > minvalue) {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Found!");
+                }
+                else if (valueSearch < middlePoint & valueSearch > minvalue) {
                     // disregard everything larger than middle point
                     maxvalue = middlePoint;
                     middlePoint = (maxvalue - minvalue) / 2;
-                        
-                } else if (valueSearch > middlePoint & valueSearch < maxvalue) {
+
+                }
+                else if (valueSearch > middlePoint & valueSearch < maxvalue) {
                     // disregard everything smaller than middle point
                     minvalue = middlePoint;
                     middlePoint = (maxvalue - minvalue) / 2 + minvalue;
-                } else {
+                }
+                else {
+                    Console.WriteLine(Environment.NewLine);
                     Console.WriteLine("Number Not Found");
-                    found = true;
+                    break;
                 }
             }
 
